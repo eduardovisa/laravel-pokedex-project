2fdeaba0ea3e88e55ae1bd1d4732a6c2f1f0c3c2
eduardovisa <eduardo.mvisa@gmail.com>
2023-08-01 15:06:22 -0600

    Model Pokemones created

---
+++ app/Models/Pokemones.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pokemones extends Model
+{
+    use HasFactory;
+}
